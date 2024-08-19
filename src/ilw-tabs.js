3a3c36b9f6efcc6efa9158b03f1556f15dfe2c04
Bryan Jonker <jonker@illinois.edu>
2024-08-19 10:13:48 -0500

    Adding functionality

--- src/ilw-tabs.js
+++ src/ilw-tabs.js
@@ -6,29 +6,150 @@ class Tabs extends LitElement {
 
     static get properties() {
         return {
-            theme: { type: String, attribute: true }
+          theme: { type: String, attribute: true },
+          width: { type: String, attribute: true },
+          default: { type: String, attribute: true },
+          horizontal: { type: Boolean, attribute: true },
+          compact: { default: false, type: Boolean, attribute: false, reflect: true }
         };
-    }
-
+      }
+    
     static get styles() {
         return styles;
     }
 
     constructor() {
         super();
-        this.align = '';
-        this.focus = '';
-        this.shadow = false;
-        this.collapse = false;
         this.theme = '';
+        this.width = '';
+        this.default = '';
+        this.horizontal = false;
+        this.handleMutation = this.handleMutation.bind(this);
+        this.handleResize = this.handleResize.bind(this);
+        this.handleTabClick = this.handleTabClick.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+        document.addEventListener('DOMContentLoaded', this.handleDocumentLoaded.bind(this));
     }
 
+    connectedCallback() {
+        super.connectedCallback();
+        if (!this.hasContainerSupport()) {
+            this.addResizeListeners();
+        }
+        this.addMutationObserver();
+    }
+    
+    disconnectedCallback() {
+        if (!this.hasContainerSupport()) {
+          this.removeResizeListeners();
+        }
+        super.disconnectedCallback();
+    }
+
+    handleDocumentLoaded() {
+        this.initializeTabs();
+        if (this.default == '') {
+            this.setActiveTab(this.querySelector('*[role="tab"]'));
+        } else {
+            this.setActiveTab(this.querySelector(`*[role="tab"][aria-controls="${this.default}"`));
+        }
+    }
+    
+    handleMutation(evt) {
+        this.initializeTabs();
+    }
+    
+    handleResize(evt) {
+        this.classList[this.isCompact() ? 'add' : 'remove']('ilw-compact');
+    }
+    
+    handleTabClick(evt) {
+        this.setActiveTab(evt.currentTarget);
+    }
+    
+    handleKeyPress(evt) {
+        if (evt.code == 'Enter' || evt.code == 'Space') {
+          this.setActiveTab(evt.currentTarget);
+        }
+    }
+
+    addMutationObserver() {
+        const observer = new MutationObserver(this.handleMutation);
+        observer.observe(this, { attributes: false, childList: true, subtree: true });
+    }
+    
+    addResizeListeners() {
+        window.addEventListener('load', this.handleResize.bind(this));
+        window.addEventListener('resize', this.handleResize.bind(this));
+    }
+    
+    getAllTabs() {
+        return this.querySelectorAll('*[role="tab"]');
+    }
+    
+    getTabPanel(tab) {
+        return document.getElementById(tab.getAttribute('aria-controls'));
+    }
+    
+    hasContainerSupport() {
+        return CSS.supports('container-type', 'inline-size');
+    }
+    
+    initializeTab(tab) {
+        if (!this.tabIsInitialized(tab)) {
+          tab.setAttribute("tabindex", 0);
+          tab.setAttribute('data-ilw-initialized', '1');
+          tab.addEventListener('click', this.handleTabClick);
+          tab.addEventListener('keydown', this.handleKeyPress);
+        }
+    }
+    
+    initializeTabs() {
+        this.getAllTabs().forEach(tab => this.initializeTab(tab));
+    }
+    
+    isCompact() {
+        return this.offsetWidth < 800;
+    }
+    
+    removeResizeListeners() {
+        window.removeEventListener('load', this.handleResize.bind(this));
+        window.removeEventListener('resize', this.handleResize.bind(this));
+    }
+    
+    setActiveTab(activeTab) {
+        console.debug(activeTab);
+        this.getAllTabs().forEach(tab => {
+          (tab === activeTab) ? this.setTabAsActive(tab) : this.setTabAsInactive(tab)
+        });
+    }
+    
+    setTabAsActive(tab) {
+        tab.setAttribute('aria-selected', 'true');
+        this.getTabPanel(tab).setAttribute('data-ilw-tab-visible', '1');
+    }
+    
+    setTabAsInactive(tab) {
+        tab.setAttribute('aria-selected', 'false');
+        this.getTabPanel(tab).setAttribute('data-ilw-tab-visible', '0');
+    }
+    
+    tabIsInitialized(tab) {
+        return tab.hasAttribute('data-ilw-initialized');
+    }
+        
     render() {
         return html`
-      <div>
-          <slot></slot>
-      </div>
-    `;
+        <div id="outer" class="${this.theme} ${this.width} ${this.horizontal ? 'horizontal' : ''}">
+        <div id="container" class="${this.theme} ${this.width}">
+            <div id="tablist" role="tablist">
+                <slot name="tabs"></slot>
+            </div>
+            <div id="tabpanels">
+                <slot></slot>
+            </div>
+        </div>
+        </div>`;
     }
 }
 
--- src/ilw-tabs.styles.js
+++ src/ilw-tabs.styles.js
@@ -1,5 +1,85 @@
 import { css } from 'lit';
 
-export default css`
+export default css `
+    #container {
+        display: var(--ilw-tabs--display);
+        background: var(--ilw-tabs--background);
+        column-gap: 60px;
+        margin: 0 auto;
+        max-width: var(--ilw-tabs--max-width);
+        padding: 60px 30px 75px;
+    }
 
+    #container.white {
+        --ilw-tabs--color: black;
+        --ilw-tabs--button--color: var(--il-blue);
+        --ilw-tabs--button--color--focus: white;
+        --ilw-tabs--button--background--focus: var(--il-blue);
+        --ilw-tabs--button--color--selected: var(--il-blue);
+        --ilw-tabs--button--background--selected: var(--il-orange);
+        --ilw-tabs--button--color--selected--focus: white;
+        --ilw-tabs--button--background--selected--focus: var(--il-blue);
+        --ilw-tabs--button--border: 2px solid var(--il-blue);
+    }
+    #container.gray {
+        --ilw-tabs--color: black;
+        --ilw-tabs--button--color: var(--il-blue);
+        --ilw-tabs--button--color--focus: white;
+        --ilw-tabs--button--background--focus: var(--il-blue);
+        --ilw-tabs--button--color--selected: var(--il-blue);
+        --ilw-tabs--button--background--selected: var(--il-orange);
+        --ilw-tabs--button--color--selected--focus: white;
+        --ilw-tabs--button--background--selected--focus: var(--il-blue);
+        --ilw-tabs--button--border: 2px solid var(--il-blue);
+    }
+
+    #outer.white {
+        --ilw-tabs--background: white;
+    }
+
+    #outer.gray {
+        --ilw-tabs--background: var(--il-storm-lighter-4);
+    }
+
+    #outer.full, #outer.auto {
+        left:50%;
+        margin-left:-50vw;
+        margin-right:-50vw;
+        padding-left:0;
+        padding-right:0;
+        position:relative;
+        right:50%;
+        width:100vw;
+        background: var(--ilw-tabs--background);
+    }
+
+    #container.auto {
+        padding: 60px max(calc(1.875rem), calc(50px - 37.5rem)) 75px;
+        max-width: 1200px;
+    }
+
+    .horizontal #tablist {
+        border-bottom: var(--ilw-tabs--button--border);
+    }
+
+    #tabpanels {
+        width: 100%;
+        margin-top: var(--ilw-tabs--tabpanels--margin-top);
+    }
+
+    @container (max-width: 800px) {
+        #container {
+            --ilw-tabs--display: block;
+            --ilw-tabs--tablist--display: flex;
+            --ilw-tabs--tablist--width: auto;
+            padding: 60px 20px 75px;
+        }
+     }
+    
+      #container.compact {
+        --ilw-tabs--display: block;
+        --ilw-tabs--tablist--display: flex;
+        --ilw-tabs--tablist--width: auto;
+        padding: 60px 20px 75px;
+    }
 `;
