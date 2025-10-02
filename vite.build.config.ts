2591ba6cfbe461a2e4164bff57aa7cb29caf90e6
Bryan Jonker <jonker@illinois.edu>
2025-10-02 13:36:13 -0500

    Major updates

---
+++ vite.build.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    root: "src",
+    build: {
+        outDir: "../dist/cdn",
+        lib: {
+            name: "ilw-tabs",
+            entry: "ilw-filter.ts",
+            fileName: "ilw-tabs",
+            formats: ["es"],
+        }
+    },
+    server: {
+        hmr: false,
+    },
+});
